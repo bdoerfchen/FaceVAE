33f74887ec8d220361b3c81e146b8d7f79ad87f5
bdoerfchen <git@bissendorf.co>
2024-01-18 16:57:33 +0100

    Introduced sampling layer

--- implementations/basic/GadVAE.py
+++ implementations/basic/GadVAE.py
@@ -1,5 +1,7 @@
 import os
 import keras
+from keras.utils import CustomObjectScope
+import tensorflow as tf
 import numpy as np
 
 # https://blog.paperspace.com/how-to-build-variational-autoencoder-keras/
@@ -19,12 +21,14 @@ class GadVAE():
         encoder_conv_2 = keras.layers.Conv2D(16, 5, activation="relu", strides=2, padding="same")(encoder_conv_1)
 
         beforeflattened_shape = keras.backend.int_shape(encoder_conv_2)[1:]
-        encoder_flatten = keras.layers.Flatten()(encoder_conv_2)
+        conv_flatten = keras.layers.Flatten()(encoder_conv_2)
 
-        encoder_mean = keras.layers.Dense(units=latent_size, name="encoder_mean")(encoder_flatten)
-        encoder_log_variance = keras.layers.Dense(units=latent_size, name="encoder_log_variance")(encoder_flatten)
+        prelatent_dense = keras.layers.Dense(4*latent_size, name="encoder_prelatent", activation="relu")(conv_flatten)
 
-        latent_space = KLDivergenceLayer()([encoder_mean, encoder_log_variance])
+        encoder_mean = keras.layers.Dense(units=latent_size, name="encoder_mean")(prelatent_dense)
+        encoder_log_variance = keras.layers.Dense(units=latent_size, name="encoder_log_variance")(prelatent_dense)
+
+        latent_space = KLDivergenceLayer()([encoder_mean, encoder_log_variance]) # Does nothing but computes the kldivergence and adds loss
 
         encoder = keras.models.Model(encoder_input, latent_space, name="encoder_model")
         encoder.summary()
@@ -32,7 +36,11 @@ class GadVAE():
         #   -----------
         #   [ Decoder ]
         decoder_input = keras.layers.Input(shape=(latent_size), name="decoder_input")
-        decoder_latent_redense = keras.layers.Dense(units=np.prod(beforeflattened_shape), name="decoder_latent_redense")(decoder_input)
+
+        decoder_postlatent = keras.layers.Dense(4*latent_size, name="decoder_postlatent", activation="relu")(decoder_input)
+
+        # Restore convoluted layers
+        decoder_latent_redense = keras.layers.Dense(units=np.prod(beforeflattened_shape), name="decoder_latent_redense", activation="relu")(decoder_postlatent)
         decoder_reshape = keras.layers.Reshape(target_shape=beforeflattened_shape)(decoder_latent_redense)
 
         decoder_deconv_1 = keras.layers.Conv2DTranspose(16, 5, activation="relu", strides=2, padding="same")(decoder_reshape)
@@ -46,18 +54,18 @@ class GadVAE():
         #   [ VAE ]
         vae_input = encoder_input
         vae_latent = encoder(vae_input)
-        vae_latent_sampled = keras.layers.Lambda(sampling, name="vae_latent_sampled")(vae_latent)
+        vae_latent_sampled = VAESamplingLayer(name="vae_latent_sampled")(vae_latent) #keras.layers.Lambda(sampling, name="vae_latent_sampled")(vae_latent)
         vae_decoder_output = decoder(vae_latent_sampled)
         vae = keras.models.Model(vae_input, vae_decoder_output, name="VAE")
         vae.summary()
-        vae.compile(optimizer=optimizer, loss=vae_loss_func(vae_latent))
+        vae.compile(optimizer=optimizer, loss=vae_reconstruction_loss)
 
         self.model = vae
         self.encoder = encoder
         self.decoder = decoder
 
-    def fit(self, x, batch_size=64, epochs=20):
-        self.model.fit(x, batch_size=batch_size, epochs=epochs)
+    def fit(self, x, *args, **kwargs):
+        self.model.fit(x, *args, **kwargs)
 
     def save(self, directory):
         self.model.save(os.path.join(directory, "vae.keras"), save_format="keras")
@@ -67,46 +75,43 @@ class GadVAE():
     def load_from_directory(directory: str):
         assert os.path.exists(directory)
         gadVae = GadVAE()
-        gadVae.model = keras.models.load_model(os.path.join(directory, "vae.keras"))
-        gadVae.encoder = keras.models.load_model(os.path.join(directory, "vae_encoder.keras"))
-        gadVae.decoder = keras.models.load_model(os.path.join(directory, "vae_decoder.keras"))
+        with CustomObjectScope({
+                'KLDivergenceLayer': KLDivergenceLayer,
+                'VAESamplingLayer': VAESamplingLayer,
+                'vae_reconstruction_loss': vae_reconstruction_loss
+            }):
+            gadVae.model = keras.saving.load_model(os.path.join(directory, "vae.keras"), safe_mode=False)
+            gadVae.encoder = keras.saving.load_model(os.path.join(directory, "vae_encoder.keras"), safe_mode=False)
+            gadVae.decoder = keras.saving.load_model(os.path.join(directory, "vae_decoder.keras"), safe_mode=False)
         return gadVae
 
     
 
-def vae_loss_func(encoder_latent):
-    def vae_reconstruction_loss(y_true, y_predict):
-        reconstruction_loss_factor = 1000
-        diff = y_true-y_predict
-        reconstruction_loss = keras.backend.square(diff)
-        reconstruction_loss = keras.backend.mean(reconstruction_loss, axis=[1, 2, 3])
+
+def vae_reconstruction_loss(y_true, y_predict):
+    reconstruction_loss_factor = 1000
+    diff = y_true-y_predict
+    reconstruction_loss = keras.backend.square(diff)
+    reconstruction_loss = keras.backend.mean(reconstruction_loss, axis=[1, 2, 3])
                 
-        l = reconstruction_loss_factor * reconstruction_loss
-        return l
+    l = reconstruction_loss_factor * reconstruction_loss
+    return l
 
-    def vae_kl_loss(y_true, y_predict):
-        #print(encoder_latent)
-        mean = encoder_latent[0]
-        log_variance = encoder_latent[1]
-        return encoder_latent[0] + encoder_latent[1]
-        #kl_loss = -0.5 * keras.backend.sum(1.0 + log_variance - keras.backend.square(mean) - keras.backend.exp(log_variance), axis=1)
-        #return kl_loss
 
-    def vae_loss(y_true, y_predict):
-        reconstruction_loss = vae_reconstruction_loss(y_true, y_predict)
-        kl_loss = vae_kl_loss(y_true, y_predict)
-        loss = reconstruction_loss# + kl_loss
+class VAESamplingLayer(keras.layers.Layer):
+    
+    def __init__(self, *args, **kwargs):
+        self.is_placeholder = True
+        super(VAESamplingLayer, self).__init__(*args, **kwargs)
 
-        return loss
+    def call(self, inputs):
+        mean, log_variance = inputs
 
-    return vae_loss
-
-def sampling(mean_log_variance):
-    mean, log_variance = mean_log_variance
-    epsilon = keras.backend.random_normal(shape=keras.backend.shape(mean), mean=0.0, stddev=1.0)
-    random_sample = mean + keras.backend.exp(log_variance/2) * epsilon
-    return random_sample
-
+        batch = keras.backend.shape(mean)[0]
+        dim = keras.backend.shape(mean)[1]
+        epsilon = keras.backend.random_normal(shape=(batch, dim))
+        return mean + keras.backend.exp(0.5 * log_variance) * epsilon
+        
         
 class KLDivergenceLayer(keras.layers.Layer):
 
@@ -121,8 +126,8 @@ class KLDivergenceLayer(keras.layers.Layer):
     def call(self, inputs):
         mean, log_variance = inputs
 
-        kl_batch = -0.5 * keras.backend.sum(1 + log_variance - keras.backend.square(mean) - keras.backend.exp(log_variance), axis=-1)
-
+        kl_batch = keras.backend.sum(-0.5 * (1 + log_variance - keras.backend.square(mean) - keras.backend.exp(log_variance)), axis=-1)
         self.add_loss(keras.backend.mean(kl_batch), inputs=inputs)
 
         return inputs
+
--- implementations/main.py
+++ implementations/main.py
@@ -1,8 +1,5 @@
 import os
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-#os.environ["DISPLAY"] = "localhost:0.0"
 
-import random
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
@@ -11,17 +8,21 @@ import keras
 
 from basic.GadVAE import GadVAE
 found_gpus = tf.config.list_physical_devices('GPU')
+print("Found GPU(s):", found_gpus)
 
 from dataset import DatasetProvider
 
 
-MODEL_FILE = "vae-basic.keras"
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 
 def main(load_model = False, save_model = True):
+
     dataset_meta = DatasetProvider.AvailableDatasets.COLORFERET
-    dataset, path = DatasetProvider.loadDataset(dataset_meta, BATCH_SIZE)
-    dataset = dataset.map(lambda x: (x/255, x/255))
+    if not load_model:
+        dataset, path = DatasetProvider.loadDataset(dataset_meta, BATCH_SIZE)
+        dataset = dataset.map(lambda x: (x/255, x/255))
+    else:
+        path = DatasetProvider.getPath(dataset_meta)
 
     val_images = [ "00084/00084_931230_fb.ppm.png", "00114/00114_931230_fb.ppm.png", "00290/00290_940422_hl.ppm.png", "00551/00551_940519_fb.ppm.png", "00826/00826_940307_fa.ppm.png" ]
     #val_images = [ "00300/00300_940422_fa.ppm.png", "00300/00300_940422_hl.ppm.png", "00300/00300_940422_pl.ppm.png", "00400/00400_940422_hl.ppm.png" ]
@@ -35,29 +36,32 @@ def main(load_model = False, save_model = True):
 
     if not load_model:
         vae = GadVAE(img_shape=(384, 256, 3), latent_size=15)
-        vae.fit(dataset, epochs=8, batch_size=BATCH_SIZE)
+        earlyStopping = keras.callbacks.EarlyStopping(monitor='loss', patience=3, min_delta=1)
+        history = vae.fit(dataset, epochs=20, batch_size=BATCH_SIZE, callbacks=[earlyStopping])
     else:
         vae = GadVAE.load_from_directory(".")
         
     if save_model:
         vae.save(".")
 
-
-    im = np.array(val_images)
-    test = np.array(vae.encoder(val_images))
-
     n_val_images = len(val_images)
     gen_images = vae.model.predict(val_images)
-    f, axes = plt.subplots(2, n_val_images)
+    f, axes = plt.subplots(2, n_val_images, num="VAE validation")
     for i in range(n_val_images):
-        axes[0, i].imshow(np.array(val_images[i]*255, dtype=np.uint8))
-        #val_image_in = np.reshape(val_images[i], newshape=(1, 384, 256, 3))
-        #gen_image = np.reshape(vae.model(val_image_in), newshape=(384, 256, 3))
-        gen_image = np.array(gen_images[i]*255, dtype=np.uint8)
-        axes[1, i].imshow(gen_image)
+        _show_image_on_axis(axes[0, i], np.array(val_images[i]*255, dtype=np.uint8))
+        gen_image = np.array(gen_images[i]*255, dtype=np.uint8) # Load image from array and denormalize
+        _show_image_on_axis(axes[1, i], gen_image)
+    axes[0, 0].set_title("Reference", loc="left")
+    axes[1, 0].set_title("Reconstructed", loc="left")
+
     plt.show()
     e = 1
 
+def _show_image_on_axis(faxis, image) -> None:
+    faxis.imshow(image)
+    faxis.get_xaxis().set_visible(False)
+    faxis.get_yaxis().set_visible(False)
+    pass
 
 
 if __name__ == "__main__":
